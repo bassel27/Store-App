8215f7f606e0edcd1b5cce9afb56740018050090
unknown <bassel_sabour@hotmail.com>
2023-01-24 12:44:35 +0200

    code refactoring

---
+++ lib/widgets/currency_and_price_text.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../models/constants.dart';
+
+class CurrencyAndPriceText extends StatelessWidget {
+  const CurrencyAndPriceText({
+    Key? key,
+    required this.price,
+  }) : super(key: key);
+
+  final double price;
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        style: Theme.of(context)
+            .textTheme
+            .bodySmall!
+            .copyWith(fontWeight: FontWeight.w300, fontSize: 13),
+        children: [
+          const TextSpan(text: "$kCurrency "),
+          TextSpan(
+              text: price.toString(),
+              style: Theme.of(context).textTheme.bodySmall),
+        ],
+      ),
+    );
+  }
+}
