0017bb5076b68219884f017e97f01c2e0d366de5
unknown <bassel_sabour@hotmail.com>
2023-02-01 11:41:12 +0200

    create one controller for orders notifier

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -163,7 +163,7 @@ class _BottomRow extends StatefulWidget {
 
 class _BottomRowState extends State<_BottomRow> {
   late final CartNotifier cartProvider = context.read<CartNotifier>();
-  late String dropdownValue;
+  late String dropdownValue = "1";
   @override
   Widget build(BuildContext context) {
     final SizeNotifier sizeProvider = Provider.of<SizeNotifier>(context);
@@ -175,7 +175,7 @@ class _BottomRowState extends State<_BottomRow> {
                 widget.product.sizeQuantity[currentlySelectedSize]!,
                 (i) => i + 1), (value) {
           dropdownValue = value;
-        }),
+        }, widget.product),
         const SizedBox(width: 6),
         Expanded(
           child: SizedBox(
@@ -207,10 +207,10 @@ class _BottomRowState extends State<_BottomRow> {
 }
 
 class DropDown extends StatefulWidget {
-  const DropDown(this.quantityList, this.onQuantityChanged);
+  const DropDown(this.quantityList, this.onQuantityChanged, this.product);
   final List quantityList;
   final Function(String) onQuantityChanged;
-
+  final Product product;
   @override
   State<DropDown> createState() => _DropDownState();
 }
@@ -235,10 +235,11 @@ class _DropDownState extends State<DropDown> {
   }
 
   String? getQuantityIfCartItemExists() {
+    SizeNotifier sizeProvider =
+        Provider.of<SizeNotifier>(context, listen: false);
     for (CartItem cartItem in cartProvider.items) {
-      if (cartItem.size ==
-          Provider.of<SizeNotifier>(context, listen: false)
-              .currentlySelectedSize) {
+      if (cartItem.size == sizeProvider.currentlySelectedSize &&
+          widget.product.id == cartItem.product.id) {
         return cartItem.quantity.toString();
       }
     }
